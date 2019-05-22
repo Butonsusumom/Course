d7edac69e693c6ea11a134123b3eeaf62e5e9fd2
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2019-05-22 12:46:17 +0300

    Add files via upload

---
+++ SkyArchiver.dpr
@@ -0,0 +1,14 @@
+program SkyArchiver;
+
+uses
+  Forms,
+  Windows,
+  MainUnit in '..\..\..\..\MainUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1,Form1);
+  Application.Run;
+end.
